3bf6120c2a0f4f5e292023b3be0ee71161d4a4d1
Rafael Grebogi <rafagrebogi@gmail.com>
2025-05-21 22:37:22 +1000

    get-user-id is working.

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi import Request
 from contextlib import asynccontextmanager
 from firebase_handler import process_training_data, process_testing_data, process_production_data,  update_server_ip
 
+from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env
+
 app = FastAPI()
+
+
+
+# -------------------------------------
+# -------------------------------------
+# Read env vars
+DB_HOST = os.getenv("SUPABASE_DB_HOST")
+DB_PORT = os.getenv("SUPABASE_DB_PORT", 6543)
+DB_NAME = os.getenv("SUPABASE_DB_NAME")
+DB_USER = os.getenv("SUPABASE_DB_USER")
+DB_PASSWORD = os.getenv("SUPABASE_DB_PASSWORD")
+
+
 
 
 @asynccontextmanager
@@ -21,6 +41,37 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def home():
     return {"message": "FastAPI ready to process data."}
+
+
+# Connect to PostgreSQL each time (could be pooled later)
+async def get_connection():
+    return await asyncpg.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+
+@app.get("/get-user-id")
+async def get_user_id(username: str = Query(...)):
+    conn = await get_connection()
+    try:
+        user = await conn.fetchrow('SELECT id FROM users WHERE "Name" = $1', username)
+        if user:
+            return {"user_id": str(user["id"])}
+        else:
+            return {"error": "User not found"}
+    finally:
+        await conn.close()
+
+
+# -------------------------------------
+# -------------------------------------
+
+
+
+
 
 @app.post("/trigger-training")
 async def trigger_training(request: Request):
